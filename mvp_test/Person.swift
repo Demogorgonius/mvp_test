d974ff282d7e4a8a04cff7cac7515df224226b79
Sergey <zvoncov@gmail.com>
2020-06-03 21:05:03 +0300

    Create structure Person

---
+++ mvp_test/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  mvp_test
+//
+//  Created by Sergey on 03.06.2020.
+//  Copyright © 2020 Sergey. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+}
