4e02693fa74092fac773fdc7f1bb9c4062ca72bc
Sergey <zvoncov@gmail.com>
2020-06-03 21:21:49 +0300

    Creating class Main presenter

--- mvp_test/MainPresenter.swift
+++ mvp_test/MainPresenter.swift
@@ -17,3 +17,20 @@ protocol MainViewPresenterProtocol: class {
     func showGreeting()
 }
 
+class MainPresenter: MainViewPresenterProtocol {
+   
+   let view: MainViewProtocol
+   let person: Person
+   
+    
+    required init(view: MainViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    func showGreeting() {
+        let greeting = self.person.firstName + " " + self.person.lastName
+        self.view.setGreeting(greeting: greeting)
+    }
+
+}
