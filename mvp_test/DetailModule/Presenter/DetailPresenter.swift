4cf8a2cac8d4fc71fd26e18bd8e609f6694dc91c
Sergey <zvoncov@gmail.com>
2020-06-04 14:50:28 +0300

    Creating detail view controller

---
+++ mvp_test/DetailModule/Presenter/DetailPresenter.swift
@@ -0,0 +1,34 @@
+//
+//  DetailPresenter.swift
+//  mvp_test
+//
+//  Created by Sergey on 04.06.2020.
+//  Copyright © 2020 Sergey. All rights reserved.
+//
+
+import Foundation
+
+protocol DetailViewProtocol: class {
+    func setComment(comment: Comment?)
+}
+
+protocol DetailViewPresenterProtocol: class {
+    init(view: DetailViewProtocol, networkService: NetworkServiceProtocol, comment: Comment?)
+    func setComment()
+}
+
+class DetailPresenter: DetailViewPresenterProtocol {
+    weak var view: DetailViewProtocol?
+    let networkService: NetworkServiceProtocol!
+    var comment: Comment?
+    
+    required init(view: DetailViewProtocol, networkService: NetworkServiceProtocol, comment: Comment?) {
+        self.view = view
+        self.networkService = networkService
+        self.comment = comment
+    }
+    
+    public func setComment() {
+        self.view?.setComment(comment: comment)
+    }
+}
